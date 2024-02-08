fa3b795e101b18fab0d43edb2cd91c4bc011da68
Harish <princeharish113@gmail.com>
2024-02-08 12:31:29 +0530

    First Commit

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+        [HttpGet("{id:int}")] 
+        public Student GetStudentbyId(int id) 
+        { 
+            return CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ StudentManagement/Models/CollegeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentManagement.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>() { 
+            new Student { Id = 113, Name = "Harish", Address = "Hubli, KA", Email = "harish@gmail.com" },
+            new Student { Id = 114, Name = "Geeta", Address = "Hubli, KA", Email = "geeta@gmail.com" }, 
+            new Student { Id = 115 } 
+        };
+    }
+}
